8219cb394e0c08a8be6052ccb1f0a233dd3e03d4
Hectordalbaz <hectordalbaz@gmail.com>
2024-06-16 20:17:09 -0600

    Se agrega componente de ver usuarios

---
+++ src/app/Componentes/usuario/ver-usuarios/ver-usuarios.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ver-usuarios',
+  standalone: true,
+  imports: [],
+  templateUrl: './ver-usuarios.component.html',
+  styleUrl: './ver-usuarios.component.css'
+})
+export class VerUsuariosComponent {
+
+}
