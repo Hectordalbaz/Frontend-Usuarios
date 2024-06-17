542b5b601ac6ea9938844908e0b1bee595fcf462
Hectordalbaz <hectordalbaz@gmail.com>
2024-06-17 11:35:30 -0600

    Enrutamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { VerSesionesComponent } from './Componentes/sesion/ver-sesiones/ver-sesiones.component';
+import { LoginComponent } from './Componentes/login/login.component';
+import { RegUsuarioComponent } from './Componentes/usuario/reg-usuario/reg-usuario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'sesiones', component:VerSesionesComponent},
+    {path: 'login',component:LoginComponent},
+    {path: 'registrar',component:RegUsuarioComponent}
+];
