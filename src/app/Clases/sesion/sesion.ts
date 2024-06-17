07d7a708c498fd258a396b67e3a06b4f549ae7c9
Hectordalbaz <hectordalbaz@gmail.com>
2024-06-17 11:40:51 -0600

    Modificacion clases de modelo

--- src/app/Clases/sesion/sesion.ts
+++ src/app/Clases/sesion/sesion.ts
@@ -1,8 +1,10 @@
+import { Usuario } from "../usuario/usuario";
+
 export class Sesion {
     constructor(
         public id_sesion: number,
-        public id_usuario: number,
-        public fec_hor_ini_ses: Date
+        public usuario: Usuario,
+        public fec_hor_ini_ses: Date,
 
     ) {}
 }
--- src/app/Clases/usuario/usuario.ts
+++ src/app/Clases/usuario/usuario.ts
@@ -1,14 +1,10 @@
 export class Usuario {
-    constructor(
-        public id_usuario: number,
-        public clave: string,
-        public nombre: string,
-        public contrasena: string,
-        public estado: boolean,
-        public fec_hor_creacion: Date,
-        public fec_hor_ult_mod: Date,
-        public usu_ult_mod: string
-    ) {}
-
-    
+        id_usuario: number;
+        clave: string;
+        nombre: string;
+        contrasena: string;
+        estado: boolean;
+        fec_hor_creacion: Date;
+        fec_hor_ult_mod: Date;
+        usu_ult_mod: string;  
 }
